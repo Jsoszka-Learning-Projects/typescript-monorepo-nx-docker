0cd9fb40baa2000653d28016d3baab2e92d5edee
Jonathan <jonathansoszka@gmail.com>
2023-01-22 00:42:42 -0500

    init

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import {fastify} from "fastify"
+
+export const app = fastify()
+
+app.get("/", (request, reply) => {
+    reply.send("Hello World")
+})
+
+
+
+
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import {app} from "./app.js"
+
+app.listen({port:3000}, () => {
+    console.log("Server Listening on port 3000")
+})
